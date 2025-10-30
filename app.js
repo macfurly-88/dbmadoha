2f9fe65bd41a2a95585d9ceb9c8d722318a3350b
macfurly-88 <mac.university8@gmail.com>
2025-10-29 22:25:23 -0600

    Actualización del app.js con conexión dbmadoha

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
 const express = require('express');
 const mysql = require('mysql');
-const myConnection = require('express-myconnection');
+
+const myConnection= require('express-myconnection');
 
 const app = express();
 
-app.use(
-  myConnection(mysql, {
-    host: 'localhost',
-    user: 'madoha',
-    password: '',
-    port: 3306,
-    database: 'dbmadoha'
-  }, 'single')
-);
+app.use(myConnection(mysql,{
+  host: 'localhost',
+  user: 'madoha',
+  password: '',
+  port: 3306,
+  database: 'dbmadoha'
+}))
 
-app.get('/', (req, res) => res.send('Servidor funcionando correctamente 🚀'));
-
-app.get('/productos', (req, res) => {
-  req.getConnection((err, conn) => {
-    if (err) return res.send(err);
-    conn.query('SELECT * FROM productos', (err, rows) => {
-      if (err) return res.send(err);
-      res.json(rows);
-    });
-  });
-});
-
-app.listen(8080, () => console.log('Servidor corriendo en http://localhost:8080'));
+app.get('/', (req, res) => res.send('Hola!'));
+app.listen(8080);
